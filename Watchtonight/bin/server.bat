5113cbb7a38c36e1df7192ef6685c1d5e9154b36
USER <user@USER-no-MacBook.local>
2021-10-05 20:11:20 +0900

    created a genie app

---
+++ Watchtonight/bin/server.bat
@@ -0,0 +1,1 @@
+"/private/var/folders/3q/94lz7_696f77g_zqm985st800000gn/T/AppTranslocation/29D46AC4-F29C-43FE-903E-A469087E7273/d/Julia-1.6.app/Contents/Resources/julia/bin/julia" --color=yes --depwarn=no --project=@. -q -i -- "%~dp0..\bootstrap.jl" s %*
